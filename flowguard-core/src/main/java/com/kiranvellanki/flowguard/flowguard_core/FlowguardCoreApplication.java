d8faa166596a41bc37185d65c351838fb53c2d6a
Kiran-Vellanki <kiranvellanki10@gmail.com>
2026-05-31 22:53:18 +0530

    Initial commit

---
+++ flowguard-core/src/main/java/com/kiranvellanki/flowguard/flowguard_core/FlowguardCoreApplication.java
@@ -0,0 +1,13 @@
+package com.kiranvellanki.flowguard.flowguard_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowguardCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowguardCoreApplication.class, args);
+	}
+
+}
---
+++ flowguard-core/src/test/java/com/kiranvellanki/flowguard/flowguard_core/FlowguardCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiranvellanki.flowguard.flowguard_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowguardCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
